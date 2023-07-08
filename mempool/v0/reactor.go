f440c34381587ec711222f4234d084be489ef499
j75689 <j75689@gmail.com>
2023-07-08 13:37:57 +0800

    chore: add mempool metrics and log

--- mempool/v0/reactor.go
+++ mempool/v0/reactor.go
@@ -187,6 +187,7 @@ func (memR *Reactor) receiveRoutine() {
 				txInfo.SenderP2PID = e.Src.ID()
 			}
 
+			memR.mempool.metrics.ReceiveTxs.Set(float64(len(protoTxs)))
 			var err error
 			for _, tx := range protoTxs {
 				ntx := types.Tx(tx)
